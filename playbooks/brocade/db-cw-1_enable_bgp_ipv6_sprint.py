1da66626b58dd1a31b3c8c0bb156f77893983611
zgould <zgould@databank.com>
2017-10-12 10:05:05 -0500

    Automation Project

---
+++ playbooks/brocade/db-cw-1_enable_bgp_ipv6_sprint.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Copyright 2015 Brocade Communications Systems, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from clicrud.device.generic import generic
+import sys
+
+#command1 = sys.argv[1]
+#command2 = sys.argv[2]
+#command3 = sys.argv[3]
+command1 = "router bgp"
+#command2 = "no neighbor 62.115.9.41 shutdown"
+command3 = "no neighbor 2600:4:2000:4::3c shutdown"
+db_cw_1 = "10.10.3.15"
+
+# With version 0.3.00, 'b64password' and 'b64enable' also exist as arguments for the below.
+# They are decoded and copied to 'password' and 'enable' automatically.
+
+transport = generic(host=db_cw_1, username="config", enable="B4nkD4t4",
+                    method="telnet", password="br@!wuRst2")
+
+print "==== Enable BGP Peering ===="
+if transport.connected:
+#   print transport.configure([command2,command3])
+    print transport.configure([command1,command3])
+
+print "\r\n==== Show Verification ===="
+# Return_type can either be a string or list. One is better for scripts,
+# the other better for automation perhaps!
+if transport.connected:
+    print transport.read("show ipv6 bgp summary | b Address", return_type="string")
+
+# print transport.protocol
+# print transport.connected
+if transport.connected:
+    transport.close()
